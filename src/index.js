3fe513c2a233146d0c871281a79b6d1f828dd170
Sonya <sonya.gromovaya2004@gmail.com>
2021-02-18 19:53:14 +0200

    fixed

--- src/index.js
+++ src/index.js
@@ -1,23 +1,25 @@
 import './styles.css';
 import menu from './menu.json';
 import menuItemTemplate from "./templates/menu-item.hbs";
+
 const ulRef = document.querySelector('.js-menu');
 const markup = menuItemTemplate(menu);
 const toggleRef = document.querySelector('.theme-switch__toggle');
 const checkedRef = document.querySelector('#theme-switch-toggle');
-ulRef.insertAdjacentHTML('beforeend', markup);
 const Theme = {
     LIGHT: 'light-theme',
     DARK: 'dark-theme',
   };
-
 const theme = localStorage.getItem('theme');
+
+ulRef.insertAdjacentHTML('beforeend', markup);
 if(!theme){
     localStorage.setItem('theme', Theme.LIGHT);
 }
 document.body.classList.add(theme);
 if(document.body.classList.contains(Theme.DARK)){
     checkedRef.checked = true;
+    document.body.classList.remove(Theme.LIGHT);
 }
 
 toggleRef.addEventListener('change', ()=>{
@@ -30,6 +32,13 @@ toggleRef.addEventListener('change', ()=>{
         localStorage.setItem('theme', Theme.LIGHT);
     }
 
+    if(document.body.classList.contains(Theme.LIGHT) && document.body.classList.contains(Theme.DARK)){
+        if(theme === Theme.LIGHT){
+            document.body.classList.remove(Theme.DARK)
+        }
+        if(theme === Theme.DARK){
+            document.body.classList.remove(Theme.LIGHT)
+        }
+    }
 
 })
-
